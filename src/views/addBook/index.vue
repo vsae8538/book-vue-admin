4e62aab82be8304fd1597c5f88ced1beaecd9cce
vsae8538 <vsae8538@gmail.com>
2021-04-23 22:11:59 +0800

    upload file features

--- src/views/addBook/index.vue
+++ src/views/addBook/index.vue
@@ -19,21 +19,65 @@
                 :value="item.id">
               </el-option>
             </el-select>
-        <!--<el-input v-model="book.categoryId" />-->
         </el-form-item>
         <el-form-item label="導讀">
           <el-input v-model="book.introduction" />
         </el-form-item>
         <el-form-item label="書籍圖片">
-          <el-input v-model="book.imageUrl" />
-        </el-form-item>
+          <el-upload
+            list-type="picture-card"
+            ref="upload"
+            action="http://localhost:8085/upload/image"
+            :on-change="handleFileChange"
+            :on-remove="handleFileRemove"
+            :on-success="uploadSuccess"
+            :on-error="uploadError"
+            :before-upload="beforeUploadFile"
+            :multiple="false"
+            :file-list="fileList"
+            :auto-upload="false"
+            accept=".jpg, .jpeg, .png, .gif"
+            >
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过20M
+          <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">確認上傳</el-button>
+          </div>
+          </el-upload>
+
         <el-form-item>
-            <el-button type="primary" @click="onTest">新增書籍</el-button>
+          <span></br></span>  
+            <el-button type="primary" @click="onSubmit">新增書籍</el-button>
         </el-form-item>
 </el-form-item>
       </el-form>
   </div>
 </template>
+
+<style>
+  .avatar-uploader .el-upload {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+  }
+  .avatar-uploader .el-upload:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: 178px;
+    height: 178px;
+    line-height: 178px;
+    text-align: center;
+  }
+  .avatar {
+    width: 178px;
+    height: 178px;
+    display: block;
+  }
+</style>
 
 <script>
 export default {
@@ -50,7 +94,8 @@ export default {
         imageUrl: ""
       },
       selectList : null,
-      selectValue: null
+      selectValue: null,
+      fileList : [],
     }
   },
 
@@ -59,17 +104,69 @@ export default {
   },
 
   methods: {
-
-    onTest(){
+    handleFileChange(file, fileList) {
+      console.log('文件改變')
+      if (fileList.length > 1) {
+        fileList.splice(0, 1)
+      }
+    },
+    handleFileRemove(file, fileList) {
+      console.log('文件移除')
+    },
+    uploadSuccess(response, file, fileList) {
       var vm = this;
-      vm.book.categoryId = vm.selectValue;
-      console.log(vm.book.categoryId);
-      console.log(vm.selectValue);
+      console.log('上傳成功')
+      console.log(response)
+      if (response.message == 'success') {
+        vm.book.imageUrl = response.data;
+        this.$message({
+          message: '文件上傳成功',
+          type: 'success'
+        })
+        console.log(vm.book.imageUrl);
+      }
+    },
+    uploadError(err, file, fileList) {
+      this.$message.error('文件上傳失敗：' + err.toString())
+    },
+    beforeUploadFile(file) {
+      const temp = file.name.substring(file.name.lastIndexOf('.') + 1)
+      if (temp !== 'jpeg' && temp !== 'jpg' && temp !== 'png' && temp !== 'gif') {
+        this.$message({
+          message: '當前文件格式不符合要求',
+          type: 'error'
+        })
+        return false
+      }
+      if (!file) {
+        this.$message({
+          message: '請選擇要上傳的文件',
+          type: 'error'
+        })
+        return false
+      }
+      if (file.size / 1024 / 1024 > 20) {
+        this.$message({
+          message: '文件大小不能超過20M',
+          type: 'error'
+        })
+        return false
+      }
+    },
+    submitUpload() {
+      this.$refs.upload.submit()
     },
 
     onSubmit() {
       var vm = this;
-      
+      if(vm.book.imageUrl == '') {
+        this.$message({
+          message: '請選擇要上傳的文件',
+          type: 'error'
+        })
+        return false
+      }
+      vm.book.categoryId = vm.selectValue;
       this.axios({
         method: 'POST',
         url: 'http://localhost:8085/book/add',
